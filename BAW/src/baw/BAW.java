4f55c9a7f6fe30d5ff8c4a750a5903acf06768e0
Nicolas <Nicolas@DESKTOP-92QHVK5>
2017-04-26 19:02:53 -0500

    Comentario del proyo

---
+++ BAW/src/baw/BAW.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package baw;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class BAW {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("La nota de la exposicion es: 5.0");
+    }
+    
+}
